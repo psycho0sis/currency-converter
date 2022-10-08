766b553e8a311790a66e8e56afa3481adc40a53c
ilonaK <ikosko@alfabank.ru>
2022-10-08 11:30:43 +0300

    refactor: refactor listOfCurrency  component

--- src/components/ListOfCurrency/index.js
+++ src/components/ListOfCurrency/index.js
@@ -4,19 +4,20 @@ import Currency from "../Currency";
 
 import "./style.scss";
 
-const ListOfCurrency = ({ currency, data, current }) => {
-  const currencies = data.map((item, i) => (
-    <Currency
-      text={item.Cur_Abbreviation}
-      key={item.Cur_ID}
-      data={item}
-      current={current}
-      img={currency[data[i].Cur_Abbreviation.toLowerCase()].img}
-      value={+currency[data[i].Cur_Abbreviation.toLowerCase()].value}
-      currency={currency}
-    />
-  ));
-  return <ul className='listOfCurrency'>{currencies}</ul>;
-};
+const ListOfCurrency = ({ currency, data, current }) => (
+  <ul className='listOfCurrency'>
+      {data.map((item, i) => (
+        <Currency
+          text={item.Cur_Abbreviation}
+          key={item.Cur_ID}
+          data={item}
+          current={current}
+          img={currency[data[i].Cur_Abbreviation.toLowerCase()].img}
+          value={+currency[data[i].Cur_Abbreviation.toLowerCase()].value}
+          currency={currency}
+        />
+      ))}
+  </ul>
+);
 
 export default ListOfCurrency;
